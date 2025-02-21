ee16ad34bbffce26e82f66d494824cf6af4083d7
van trong <trongdn2405@gmail.com>
2025-02-21 21:26:42 +0700

    feat: category crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,9 @@ import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './routes/auth/auth.module';
 // import { CatchEverythingFilter } from './shared/common/http-exception.filter'
 import { CoursesModule } from './routes/course/course.module';
+import { CategoryModule } from './routes/category/category.module';
 @Module({
-  imports: [SharedModule, AuthModule, CoursesModule],
+  imports: [SharedModule, AuthModule, CoursesModule, CategoryModule],
   controllers: [AppController],
   providers: [
     AppService,
---
+++ src/routes/category/category.controller.ts
@@ -0,0 +1,46 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, ParseIntPipe, DefaultValuePipe, BadRequestException } from '@nestjs/common';
+import { CategoryService } from './category.service';
+
+@Controller('category')
+export class CategoryController {
+    constructor(
+        private readonly categoryService: CategoryService
+    ) { }
+
+    @Get()
+    async getCategories(
+        @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
+        @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number = 10,
+    ) {
+        if (page < 1) {
+            throw new BadRequestException('Page number must be greater than 0');
+        }
+        if (limit < 1 || limit > 100) {
+            throw new BadRequestException('Limit must be between 1 and 100');
+        }
+        return await this.categoryService.getCategories({ page, limit });
+    }
+
+    @Get('/:id')
+    async getCategoryById(@Param('id') id: string) {
+        return await this.categoryService.getCategoryById(Number(id));
+    }
+
+    @Post()
+    async createCategory(@Body() body: { name: string; description?: string }) {
+        return await this.categoryService.createCategory(body);
+    }
+
+    @Patch('/:id')
+    async updateCategory(
+        @Param('id') id: string,
+        @Body() body: { name?: string; description?: string }
+    ) {
+        return await this.categoryService.updateCategory(Number(id), body);
+    }
+
+    @Delete('/:id')
+    async deleteCategory(@Param('id') id: string) {
+        return await this.categoryService.deleteCategory(Number(id));
+    }
+}
